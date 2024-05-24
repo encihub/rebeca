fb352a330ce6c3360554ef386f8bf4d2161caead
Javier López Úbeda <jlopezcur@gmail.com>
2024-05-24 23:36:04 +0200

    feat: rewriting services

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,18 @@
-import type { GatsbyConfig, PluginRef } from "gatsby"
-import "dotenv/config"
-
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+import type { GatsbyConfig, PluginRef } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-emma-core/gatsby-config.mjs
-    siteTitle: `Emma`,
-    siteTitleAlt: `Emma - Gatsby Starter Portfolio`,
-    siteHeadline: `Emma - Gatsby Theme from @lekoarts`,
+    siteTitle: `Rebeca`,
+    siteTitleAlt: `Rebeca`,
+    siteHeadline: `Rebeca`,
     siteUrl: `https://emma.lekoarts.de`,
     siteDescription: `Minimalistic portfolio with full-width grid, page transitions, support for additional MDX pages, and a focus on large images`,
     siteImage: `/banner.jpg`,
-    siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    siteLanguage: `es`,
+    author: `@jlopezcur`,
   },
   trailingSlash: `always`,
   plugins: [
@@ -33,8 +30,8 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emma - @lekoarts/gatsby-theme-emma`,
-        short_name: `Emma`,
+        name: `Rebeca`,
+        short_name: `Rebeca`,
         description: `Minimalistic bright portfolio with full-width grid and large images`,
         start_url: `/`,
         background_color: `#fff`,
@@ -56,16 +53,7 @@ const config: GatsbyConfig = {
         ],
       },
     },
-    // You can remove this plugin if you don't need it
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-statoscope`,
-      options: {
-        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
-        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
-        open: false,
-      },
-    },
   ].filter(Boolean) as Array<PluginRef>,
-}
+};
 
-export default config
+export default config;
