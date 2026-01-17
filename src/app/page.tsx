bf13da10757dd06f6b8a5ff35ea3f7406212232c
Javier López Úbeda <jlopezcur@gmail.com>
2026-01-18 00:21:43 +0100

    feat: change main grid to 2 cols

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { HomeItem } from '@components';
 export default async () => {
   const services = await getServices();
   return (
-    <div className="grid grid-col-1 lg:grid-cols-3">
+    <div className="grid grid-col-1 lg:grid-cols-2">
       {services &&
         services.map((service, index) => (
           <HomeItem key={service.filename} service={service} index={index} />
